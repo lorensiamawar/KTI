f5943c95e586bc858977cb0fc52ec100307bd7a3
Mawar <Mawar@LORENSIAMAWAR>
2016-12-12 21:55:58 +0700

    Add project files.

---
+++ KTI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KTI.Startup))]
+namespace KTI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
